67a72c83591518a8b81b9a185b84593220cb0239
Kirill <mr.microb32@gmail.com>
2021-03-21 01:55:35 +0300

    first commit

---
+++ BilliardGame/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilliardGame
+{
+    public class GameField : Form1
+    {
+        protected int BorederWidth = 10;
+    }
+   public class Ball : GameField
+    {
+         public float X { get; set;}
+         public float Y { get; set;}
+         public float R { get; set; }
+         float DX { get; set;}
+         float DY { get; set;}
+        public  Ball(float x, float y, float r, float dx, float dy)
+        {
+            this.X = x;
+            this.Y = y;
+            this.R = r;
+            this.DX = dx;
+            this.DY = dy;
+        }
+
+        public void MoveBall()
+        {
+            if (X >= Form1.ActiveForm.ClientSize.Width - 2 * R)
+                DX = -DX;
+            if(X<0)
+                DX = -DX;
+            X += DX;
+            DX *= 0.99F;
+            if (Y >= Form1.ActiveForm.ClientSize.Height - 2 * R)
+                DY = -DY;
+            if (Y < 0)
+                DY = -DY;
+            Y += DY;
+            DY *= 0.99F;
+        }
+
+    }
+}
---
+++ BilliardGame/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BilliardGame
+{
+    public partial class Form1 : Form
+    {
+        
+        Graphics g;
+        Bitmap btm;
+        Graphics BtmToG;
+        public Ball ball;
+        SolidBrush br = new SolidBrush(Color.Blue);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            btm = new Bitmap(this.Width, this.Height);
+            BtmToG = Graphics.FromImage(btm);
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        }
+        
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            BtmToG.Clear(Color.Green);
+            ball.MoveBall();       // вычисление нового положения шара
+            BtmToG.FillEllipse(br, ball.X, ball.Y, 2 * ball.R, 2 * ball.R);
+            g.DrawImage(btm, Point.Empty);
+            
+            label1.Text = Convert.ToString(ball.X);
+            label2.Text = Convert.ToString(ball.Y);
+           // label3.Text = Convert.ToString(ball1.D);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ball = new Ball(122F, 222F, 15F, 16f, -13f);
+            timer1.Enabled = true;
+        }
+    }
+}
